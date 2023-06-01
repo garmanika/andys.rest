ac9db1cf89379e30aeae3fbee48b65d62e94ca47
Garmanika <aleks.orlov97@gmail.com>
2023-06-01 18:05:38 +0300

    01/06

--- app/js/main.js
+++ app/js/main.js
@@ -26,7 +26,7 @@ class HeaderUi {
   }
 
   initScrollEvents() {
-    let pageHeader = document.querySelector(".template-header");
+    let pageHeader = document.querySelector(".header");
     window.onscroll = function () {
       if (this.oldScroll > this.scrollY) {
         pageHeader.classList.remove("template-header--hidden");
@@ -139,6 +139,7 @@ class HeaderUi {
   }
 }
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const headerUi = new HeaderUi(
     ".header-menu-mobile-container",
@@ -156,4 +157,204 @@ document.addEventListener("DOMContentLoaded", function () {
       header.classList.remove("sticky");
     }
   });
+  if (document.querySelector('.index-banner-slider .swiper') !== null) {
+    let aboutAwardsSlider = new Swiper('.index-banner-slider .swiper', {
+      slidesPerView: 'auto',
+      slidesPerGroup: 1,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      pagination: {
+        el: ".index-banner-slider .swiper-pagination",
+        type: "bullets",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.index-banner-slider .swiper-button-next',
+        prevEl: '.index-banner-slider .swiper-button-prev',
+      },
+    });
+  }
+  if (document.querySelector('.text-page-slider .swiper') !== null) {
+    const swiper = new Swiper(".text-page-slider .swiper", {
+
+
+      pagination: {
+        el: ".text-page-slider .swiper-pagination",
+        type: "bullets",
+        clickable: true,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+          slidesPerGroup: 1,
+        },
+        769: {
+          slidesPerView: 2,
+          spaceBetween: 32,
+          slidesPerGroup: 2,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 32,
+          slidesPerGroup: 3,
+        },
+      },
+    });
+  }
+  if (document.querySelector('.menu-detail-slider .swiper') !== null) {
+    const swiper = new Swiper(".menu-detail-slider .swiper", {
+
+
+
+      pagination: {
+        el: ".menu-detail-slider .swiper-pagination",
+        type: "bullets",
+        clickable: true,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+          slidesPerGroup: 1,
+        },
+        769: {
+          slidesPerView: 2,
+          spaceBetween: 32,
+          slidesPerGroup: 2,
+        },
+        991: {
+          slidesPerView: 3,
+          spaceBetween: 32,
+          slidesPerGroup: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 32,
+          slidesPerGroup: 4,
+        },
+      },
+    });
+  }
+  function createContactsMap(containerId, data = {}) {
+    ymaps.ready(function () {
+      let map = new ymaps.Map(containerId, {
+        center: data.mapCenter,
+        zoom: data.mapZoom,
+        controls: ["zoomControl"],
+      });
+      let geoCollection = new ymaps.GeoObjectCollection();
+      for (let i = 0; i < data.mapPoints.length; i++) {
+        geoCollection.add(
+          new ymaps.Placemark(
+            data.mapPoints[i],
+            {},
+            {
+              iconLayout: "default#image",
+              iconImageHref: "img/svg/map-icon.svg",
+              iconImageSize: [48, 48],
+            }
+          )
+        );
+      }
+
+      map.geoObjects.add(geoCollection);
+      map
+        .setBounds(map.geoObjects.getBounds(), {
+          zoomMargin: [0, 13],
+          checkZoomRange: true,
+        })
+        .then(function () {
+          if (map.getZoom() > 13) map.setZoom(13);
+        });
+    });
+  }
+
+
+  let detailMapData = {
+    mapCenter: [54.51649007011178, 36.25665149999999],
+    mapZoom: 17,
+    mapPoints: [
+      [54.51649007011178, 36.25665149999999],
+      // [54.52, 36.25],
+    ],
+  };
+
+  if (document.querySelector("#map").length > 0) {
+    createContactsMap("map", detailMapData);
+  }
+  createContactsMap("map", detailMapData);
+  if (document.querySelector("#contact-map").length > 0) {
+    createContactsMap("contact-map", detailMapData);
+  }
+  createContactsMap("contact-map", detailMapData);
+  let phoneInputs = document.querySelectorAll('input[type="tel"]');
+  for (let phoneInput of phoneInputs) {
+    IMask(phoneInput, {
+      'mask': '+{7} (000) 000-00-00'
+    });
+  }
+
+  flatpickr(".web-form-item-data-time input", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+  });
+  flatpickr(".web-form-item-data input", {});
+  document.querySelectorAll(".menu-section-item-quantity-icon").forEach(function (element) {
+    element.addEventListener("click", function () {
+      var parentBox = this.parentNode;
+      parentBox.classList.add("active");
+    });
+  });
+
+
+  let accordionItems = document.querySelectorAll('.accordion-item');
+  for (let accordionItem of accordionItems) {
+    let accordionItemHeading = accordionItem.querySelector('.accordion-item-heading');
+    accordionItemHeading.addEventListener('click', function () {
+      accordionItem.classList.toggle('active');
+      if (accordionItem.dataset.accordionGroup) {
+        for (let groupItem of document.querySelectorAll('.accordion-item[data-accordion-group="' + accordionItem.dataset.accordionGroup + '"]')) {
+          if (accordionItem !== groupItem) {
+            groupItem.classList.remove('active');
+          }
+        }
+      }
+    });
+  }
+  const tx = document.getElementsByTagName("textarea");
+  for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+  }
+
+  function OnInput() {
+    this.style.height = 0;
+    this.style.height = (this.scrollHeight) + "px";
+  }
+
+    var clearButtons = document.querySelectorAll('.web-form-item-clear');
+
+    clearButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        var formItemControl = button.closest('.web-form-item.control');
+        var textarea = formItemControl.querySelector('textarea');
+        textarea.value = '';
+        textarea.focus();
+      });
+    });
+
+    var undisButtons = document.querySelectorAll('.web-form-item-undis');
+
+    undisButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        var formItemControl = button.closest('.web-form-item.control');
+        var textarea = formItemControl.querySelector('textarea');
+        textarea.disabled = !textarea.disabled;
+        textarea.focus();
+      });
+    });
+
 });
